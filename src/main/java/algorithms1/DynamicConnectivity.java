77a099bc9fe5de9232ce74a4ef305dc19290bad8
Muthu Palanisamy <muthazhagu@gmail.com>
2016-12-28 01:38:38 -0500

    Determining dynamic connectivity.

---
+++ src/main/java/algorithms1/DynamicConnectivity.java
@@ -0,0 +1,29 @@
+package algorithms1;
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.UF;
+
+import java.io.File;
+
+/**
+ * Created by muthu on 12/28/16.
+ */
+public class DynamicConnectivity {
+    public static void main(String[] args) {
+        File file = new File(args[0]);
+        In in = new In(file);
+        int N = in.readInt();
+        UF uf = new UF(N);
+        while (in.hasNextLine()) {
+            int p = in.readInt();
+            int q = in.readInt();
+            if (!uf.connected(p, q)) {
+                uf.union(p, q);
+                StdOut.println(p + " " + q);
+            } else {
+                StdOut.println(p + " -NOT CONNECTED- " + q);
+            }
+        }
+    }
+}

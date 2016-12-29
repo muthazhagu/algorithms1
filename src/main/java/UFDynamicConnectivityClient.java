61e31db9a73447a9fdd6ebd7ff27a657fae598f4
Muthazhagu (Muthu) Palanisamy <muthazhagu@gmail.com>
2016-12-29 03:45:28 -0500

    checkstyle fixes.

--- src/main/java/UFDynamicConnectivityClient.java
+++ src/main/java/UFDynamicConnectivityClient.java
@@ -1,3 +1,4 @@
+
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.UF;
@@ -11,15 +12,16 @@ public class UFDynamicConnectivityClient {
     public static void main(String[] args) {
         File file = new File(args[0]);
         In in = new In(file);
-        int N = in.readInt();
-        UF uf = new UF(N);
+        int n = in.readInt();
+        UF uf = new UF(n);
         while (in.hasNextLine()) {
             int p = in.readInt();
             int q = in.readInt();
             if (!uf.connected(p, q)) {
                 uf.union(p, q);
                 StdOut.println(p + " " + q);
-            } else {
+            }
+            else {
                 StdOut.println(p + " -NOT CONNECTED- " + q);
             }
         }

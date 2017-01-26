6fce4533ed798609f44d9e7f7fdb03633b72b702
Muthazhagu (Muthu) Palanisamy <muthazhagu@gmail.com>
2017-01-26 00:24:27 -0500

    Code for a RandomizedQueue.

---
+++ src/main/java/RandomizedQueue.java
@@ -0,0 +1,96 @@
+
+import edu.princeton.cs.algs4.StdRandom;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class RandomizedQueue<Item> implements Iterable<Item> {
+    private Item[] q = (Item[]) new Object[1];
+    private int n = 0;
+    private int tail = 0;
+
+    // construct an empty randomized queue
+    public RandomizedQueue() {
+
+    }
+
+    // is the queue empty?
+    public boolean isEmpty() {
+        return n == 0;
+    }
+
+    // return the number of items on the queue
+    public int size() {
+        return n;
+    }
+
+    // add the item
+    public void enqueue(Item item) {
+        if (item == null) {
+            throw new NullPointerException();
+        }
+
+        if (n == q.length) {
+            resize(2 * q.length);
+        }
+
+        q[tail] = item;
+        n += 1;
+        tail += 1;
+    }
+
+    // remove and return a random item
+//    public Item dequeue() {
+//        if (isEmpty()) {
+//            throw new NoSuchElementException();
+//        }
+//    }
+
+    // return (but do not remove) a random item
+    public Item sample() {
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+
+        return q[StdRandom.uniform(n)];
+    }
+
+    // return an independent iterator over items in random order
+    public Iterator<Item> iterator() {
+        return new RandomizedQueueIterator();
+    }
+
+    private void resize(int size) {
+        Item[] temp = (Item[]) new Object[size];
+
+        for (int i = 0; i < n; i++) {
+            temp[i] = q[i];
+        }
+
+        q = temp;
+    }
+
+    private class RandomizedQueueIterator implements Iterator<Item> {
+
+        private int index = 0;
+
+        @Override
+        public boolean hasNext() {
+            return index != n;
+        }
+
+        @Override
+        public Item next() {
+            if (!hasNext()) throw new NoSuchElementException();
+
+            Item item = q[index];
+            index += 1;
+            return item;
+        }
+
+        @Override
+        public void remove() {
+            throw new NoSuchElementException();
+        }
+    }
+}
---
+++ src/test/java/RandomizedQueueTest.java
@@ -0,0 +1,75 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Created by muthu on 1/25/17.
+ */
+public class RandomizedQueueTest {
+    private RandomizedQueue<String> q;
+
+    @Before
+    public void setup() {
+        q = new RandomizedQueue<>();
+        assert q.isEmpty();
+    }
+
+    @Test
+    public void enqOneItem() {
+        q.enqueue("One");
+        assert !q.isEmpty();
+        assert q.size() == 1;
+        assert q.sample().equals("One");
+
+        Iterator myIterator = q.iterator();
+
+        while (myIterator.hasNext()) {
+            assert myIterator.next().equals("One");
+        }
+    }
+
+    @Test
+    public void enqTwoItems() {
+        q.enqueue("One");
+        q.enqueue("Two");
+        assert q.size() == 2;
+
+        Iterator myIterator = q.iterator();
+
+        while (myIterator.hasNext()) {
+            assert myIterator.next().equals("One");
+            assert myIterator.next().equals("Two");
+        }
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void addNullItemTest() {
+        q.enqueue(null);
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void sampleWhenEmptyTest() {
+        q.sample();
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void iteratorRemoveTest() {
+        q.enqueue("One");
+
+        Iterator myIterator = q.iterator();
+        myIterator.remove();
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void iteratorNextTest() {
+        Iterator myIterator = q.iterator();
+        myIterator.next();
+
+        q.enqueue("One");
+
+        assert myIterator.next().equals("One");
+        myIterator.next();
+    }
+}
